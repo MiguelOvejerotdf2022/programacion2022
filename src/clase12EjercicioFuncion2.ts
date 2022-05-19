a62dc4ab759c8840dec911b19f11d8cc49ef2c35
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-19 11:40:37 -0300

    correccion de variables para que no se crucen

--- src/clase12EjercicioFuncion2.ts
+++ src/clase12EjercicioFuncion2.ts
@@ -1,8 +1,8 @@
 /*crear una funcion que reciba un valor numerico y
 devuelva true si es par o false si es impar (con retorno)*/
 
-function parImpar(num: number): boolean {
-  if (num % 2 === 0) {
+function parImpar(num3: number): boolean {
+  if (num3 % 2 === 0) {
     console.log("El numero ingresado es PAR");
     return true;
   } else {
@@ -11,7 +11,7 @@ function parImpar(num: number): boolean {
   }
 }
 
-let num: number = Number(prompt("Ingrese un Numero: "));
-let resultado: boolean = parImpar(num);
+let num3: number = Number(prompt("Ingrese un Numero: "));
+let resultado3: boolean = parImpar(num3);
 
-console.log(resultado);
+console.log(resultado3);
