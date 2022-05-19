3d0266b136a8dc6d8bfcbec4e718cfa9553e4336
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-19 11:39:10 -0300

    Correccion de string

--- src/clase12EjercicioFuncion.ts
+++ src/clase12EjercicioFuncion.ts
@@ -1,10 +1,13 @@
 /*Crear un programa con una funcion que reciba por parametro su nombre 
 y apellido y lo imprima por consola.*/
 
-function nombreYApellido(nombre: string, apellido: string): void {
-  console.log("Nombre y Apellido: " + nombre + " " + apellido);
+function nombreYApellido(nombre: string, apellido: string): string {
+  let concatenacion: string = nombre + " " + apellido;
+  return concatenacion;
 }
 
-let nombre: string = prompt("Ingrese su Nombre: ");
-let apellido: string = prompt("Ingrese su Apellido: ");
-nombreYApellido(nombre, apellido);
+let nombre: string = "Miguel Orlando";
+let apellido: string = "Ovejero Alvarez";
+let resultado2: string = nombreYApellido(nombre, apellido);
+
+console.log("Nombre y Apellido: " + resultado2);
--- src/clase12EjercicioFuncion2.ts
+++ src/clase12EjercicioFuncion2.ts
@@ -2,21 +2,16 @@
 devuelva true si es par o false si es impar (con retorno)*/
 
 function parImpar(num: number): boolean {
-  
   if (num % 2 === 0) {
+    console.log("El numero ingresado es PAR");
     return true;
   } else {
+    console.log("El numero ingresado es IMPAR");
     return false;
   }
-}  
+}
 
 let num: number = Number(prompt("Ingrese un Numero: "));
 let resultado: boolean = parImpar(num);
 
-if(resultado ===true){
-  console.log(resultado);
-  console.log("El numero ingresado es PAR");
-}else{
-  console.log(resultado);
-  console.log("El numero ingresado es IMPAR");
-}
+console.log(resultado);
