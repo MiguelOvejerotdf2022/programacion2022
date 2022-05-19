690a238f886ce759a0d843e8d869db90bf0ea13a
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-18 23:03:23 -0300

    clase12EjercicioFuncion2.ts se agrego if else, muestro mensaje

--- src/clase12EjercicioFuncion2.ts
+++ src/clase12EjercicioFuncion2.ts
@@ -2,14 +2,21 @@
 devuelva true si es par o false si es impar (con retorno)*/
 
 function parImpar(num: number): boolean {
+  
   if (num % 2 === 0) {
     return true;
   } else {
     return false;
   }
-}
+}  
 
 let num: number = Number(prompt("Ingrese un Numero: "));
 let resultado: boolean = parImpar(num);
 
-console.log(resultado);
+if(resultado ===true){
+  console.log(resultado);
+  console.log("El numero ingresado es PAR");
+}else{
+  console.log(resultado);
+  console.log("El numero ingresado es IMPAR");
+}
