77f946da086c9362ce75292333ee01cd1937ebbc
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-22 20:50:03 -0300

    clase14DosArreglos 5.Arreglos Pag 42

---
+++ clase14DosArreglos.ts
@@ -0,0 +1,5 @@
+/*• Construya un algoritmo que tenga dos arreglos uno que almacene
+2 nombres y otro que almacene 3 números ambos ingresados por el usuario.
+• Al final debe imprimir los valores por consola.*/
+
+let 2Nombres:string[] = new Array(2);
---
+++ src/clase14DosArreglos.ts
@@ -0,0 +1,20 @@
+/*• Construya un algoritmo que tenga dos arreglos uno que almacene
+2 nombres y otro que almacene 3 números ambos ingresados por el usuario.
+• Al final debe imprimir los valores por consola.*/
+
+let cargarNombres: string[] = new Array(2);
+let cargarNumeros: number[] = new Array(3);
+
+for (let indice: number = 0; indice < 2; indice++) {
+  cargarNombres[indice] = prompt("Ingrese Nombre y Apellido: ");
+}
+for (let indice: number = 0; indice < 2; indice++) {
+  console.log(indice + 1 + " ---> " + cargarNombres[indice]);
+}
+
+for (let indice: number = 0; indice < 3; indice++) {
+  cargarNumeros[indice] = Number(prompt("Inrese Numero: "));
+}
+for (let indice: number = 0; indice < 3; indice++) {
+  console.log(indice + 1 + " ---> " + cargarNumeros[indice]);
+}
