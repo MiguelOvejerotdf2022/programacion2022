8647edda5d427f9e026e0237ea3e5d29226dd112
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-22 15:08:20 -0300

    modificacion

--- src/arregloDeNumeros.ts
+++ src/arregloDeNumeros.ts
@@ -7,5 +7,5 @@ datos: 20, 14, 8, 0, 5, 19 y 24.
 let arregloNum: number[] = [20, 14, 8, 0, 5, 19, 24];
 
 for (let indice: number = 0; indice < 7; indice++) {
-  console.log(arregloNum[indice]);
+  console.log("Posiocion " + (indice + 1) + " ----> " + arregloNum[indice]);
 }
