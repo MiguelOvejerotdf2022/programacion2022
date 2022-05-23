e01f4fd84f7fc91ff61590ce6a2a8b1a96071682
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-23 00:25:39 -0300

    Clase14 Ejercicio CompletarArreglo 5.Arreglos Pag48

--- src/clase14CompletarArreglo.ts
+++ src/clase14CompletarArreglo.ts
@@ -2,4 +2,17 @@
 • Para obtener números aleatorios crear una función Azar,
 que se base en las funciones disponibles en el paquete Math:
 -Math.random() devuelve un nro al azar entre 0 y 1.*/
-let Array10: number[] = new Array(10);
+function azar(): number {
+  return Math.floor(Math.random() * 100);
+}
+
+let array10: number[] = new Array(10);
+let indice: number = 0;
+
+for (indice = 0; indice < 10; indice++) {
+  array10[indice] = azar();
+}
+
+for (indice = 0; indice < 10; indice++) {
+  console.log("POSICION " + (indice + 1) + " ---> " + array10[indice]);
+}
