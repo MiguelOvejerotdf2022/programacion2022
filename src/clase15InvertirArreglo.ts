e92fa3e847d98a199df7b6af8470216a052b2bf9
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-25 15:25:41 -0300

    agrego comentarios

--- src/clase15InvertirArreglo.ts
+++ src/clase15InvertirArreglo.ts
@@ -5,20 +5,23 @@ Ejemplo:
 v = 1, 3, 7, 9, 9, 5
 La salida es: 5, 9, 9, 7, 3, 1*/
 
-let longitud: number = Number(prompt("Ingrese Dimension del Arreglo: "));
-let arregloInvertido: number[] = new Array(longitud);
+let longitud: number = Number(prompt("Ingrese Dimension del Arreglo: ")); //Declaro variable y se pide al usuario la cantidad de elementos que va a contener el arreglo.
+let arregloInvertido: number[] = new Array(longitud); //Declaro el arreglo y le asigno la dimension de elementos.
 
 for (let indice: number = 0; indice < longitud; indice++) {
+  //Cargo el Arreglo.
   arregloInvertido[indice] = Number(
     prompt("Posicion " + (indice + 1) + " Ingrese elemento: ")
   );
 }
 
 for (let indice: number = 0; indice < longitud; indice++) {
+  //Imprimo el arreglo antes de invertirlo para comprobar.
   console.log("Posicion " + (indice + 1) + " -> " + arregloInvertido[indice]);
 }
 
 console.log("ARREGLO INVERTIDO ----");
 for (let indice: number = longitud - 1; indice >= 0; indice--) {
+  //Invierto el Arreglo.
   console.log("Posicion " + (indice + 1) + " -> " + arregloInvertido[indice]);
 }
