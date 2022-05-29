a4ed1ac7e86f7a8ff81d5b87d60bc72907056664
MiguelOvejerotdf2022 <104174273+MiguelOvejerotdf2022@users.noreply.github.com>
2022-05-28 23:37:58 -0300

    busqueda secuencial

--- src/TareaSumar2Arreglos.ts
+++ src/TareaSumar2Arreglos.ts
@@ -2,19 +2,46 @@
 y guardar el resultado en otro arreglo
 • El arreglo tiene dimensión 6 y los números de los dos vectores
 los carga el usuario*/
+
+function cargarVector(parametro: number[]): void {
+  for (let indice: number = 0; indice < parametro.length; indice++) {
+    parametro[indice] = Number(prompt("Posicion " + (indice + 1)));
+  }
+}
+
+function sumarVectores(
+  vector1: number[],
+  vector2: number[],
+  vectorsuma: number[]
+): void {
+  for (let indice: number = 0; indice < vector1.length; indice++) {
+    vectorsuma[indice] = vector1[indice] + vector2[indice];
+  }
+}
+
+function MostrarSuma(muestroSuma: number[]): void {
+  for (let indice: number = 0; indice < muestroSuma.length; indice++) {
+    console.log(muestroSuma[indice]);
+  }
+}
+
 let dimension: number = Number(prompt("DIMENSION de los vectores: "));
 let arreglo1: number[] = new Array(dimension);
 let arreglo2: number[] = new Array(dimension);
 let arregloSuma: number[] = new Array(dimension);
 
-for (let indice: number = 0; indice < dimension; indice++) {
-  //Cargo datos Vector 1, 2 y Vector Suma.
+/*for (let indice: number = 0; indice < dimension; indice++) {  //Cargo datos Vector 1, 2 y Vector Suma.
   arreglo1[indice] = Number(prompt("VECTOR 1 - Posicion " + (indice + 1)));
   arreglo2[indice] = Number(prompt("VECTOR 2 - Posicion " + (indice + 1)));
   arregloSuma[indice] = arreglo1[indice] + arreglo2[indice];
-}
+}*/
 
-for (let indice: number = 0; indice < dimension; indice++) {
+cargarVector(arreglo1);
+cargarVector(arreglo2);
+sumarVectores(arreglo1, arreglo2, arregloSuma);
+MostrarSuma(arregloSuma);
+
+/*for (let indice: number = 0; indice < dimension; indice++) {
   //Muestro Datos cargados.
   console.log(
     "[ VECTOR 1 ] posicion " + (indice + 1) + " = " + arreglo1[indice]
@@ -25,4 +52,4 @@ for (let indice: number = 0; indice < dimension; indice++) {
   console.log(
     "[ VECTOR SUMA ] posicion " + (indice + 1) + " --> " + arregloSuma[indice]
   );
-}
+}*/
--- src/tareaPromedioEscolar.ts
+++ src/tareaPromedioEscolar.ts
@@ -5,21 +5,34 @@ de un alumno (ingresado por el usuario)
 Luego de resolverlo, pensar en aprovechar métodos y discutir como representar
 la información. */
 
-function cargaDeDatos(
-  arregloNom: string[],
-  arregloTri1: number[],
-  arregloTri2: number[],
-  arregloTri3: number[]
-): void {
-  for (let indice: number = 0; indice < arregloNom.length; indice++) {
-    arregloNom[indice] = prompt(
+function cargarVectorNombres(parametroNombres: string[]): void {
+  for (let indice: number = 0; indice < parametroNombres.length; indice++) {
+    parametroNombres[indice] = prompt(
       "Ingrese Nombre del Alumno " + (indice + 1) + ": "
     );
+  }
+}
+
+function cargarVectorNotas(
+  nota1: number[],
+  nota2: number[],
+  nota3: number[]
+): void {
+  for (let indice: number = 0; indice < nota1.length; indice++) {
+    nota1[indice] = Number(prompt("Ingrese 1° Nota: "));
+    nota2[indice] = Number(prompt("Ingrese 2° Nota: "));
+    nota3[indice] = Number(prompt("Ingrese 3° Nota: "));
+  }
+}
+
+/*function cargaDeDatos(arregloNom:string[],arregloTri1:number[],arregloTri2:number[],arregloTri3:number[]):void{
+  for (let indice:number = 0; indice < arregloNom.length; indice++) {
+    arregloNom[indice] = prompt("Ingrese Nombre del Alumno " + (indice+1)+ ": ");
     arregloTri1[indice] = Number(prompt("Ingrese 1° Nota: "));
     arregloTri2[indice] = Number(prompt("Ingrese 2° Nota: "));
     arregloTri3[indice] = Number(prompt("Ingrese 3° Nota: "));
   }
-}
+}*/
 
 function cargarYMostrarPromedio(
   arregloTri1: number[],
@@ -51,13 +64,12 @@ let arreglo_Trimestre1: number[] = new Array(longitud_Arreglo);
 let arreglo_Trimestre2: number[] = new Array(longitud_Arreglo);
 let arreglo_Trimestre3: number[] = new Array(longitud_Arreglo);
 let arreglo_Promedio: number[] = new Array(longitud_Arreglo);
+let buscarAlumno: string;
 
-cargaDeDatos(
-  arreglo_Nombres,
-  arreglo_Trimestre1,
-  arreglo_Trimestre2,
-  arreglo_Trimestre3
-);
+cargarVectorNombres(arreglo_Nombres);
+cargarVectorNotas(arreglo_Trimestre1, arreglo_Trimestre2, arreglo_Trimestre3);
+
+//cargaDeDatos(arreglo_Nombres,arreglo_Trimestre1,arreglo_Trimestre2,arreglo_Trimestre3);
 console.log("** ALUMNOS -- NOTAS -- PROMEDIO FINAL **");
 cargarYMostrarPromedio(
   arreglo_Trimestre1,
@@ -66,3 +78,21 @@ cargarYMostrarPromedio(
   arreglo_Promedio,
   arreglo_Nombres
 );
+
+buscarAlumno = prompt("Ingrese el Nombre del Alumno a buscar: ");
+
+for (
+  let indice: number = 0;
+  indice < arreglo_Nombres.length && arreglo_Nombres[indice] !== buscarAlumno;
+  indice++
+);
+if (indice === arreglo_Nombres.length) {
+  console.log("NO se encontro el Alumno que buscaba");
+} else {
+  console.log(
+    "Se encontro el Alumno que buscaba: " +
+      arreglo_Nombres[indice] +
+      " con PROMEDIO de " +
+      arreglo_Promedio[indice]
+  );
+}
